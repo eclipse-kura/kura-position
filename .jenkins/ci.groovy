c2ab396999367e171153afc0a196360c0e313e01
Mattia Dal Ben <mattdibi@users.noreply.github.com>
2025-07-30 09:35:39 +0200

    ci: migrate to shared CI pipeline (#14)

---
+++ .jenkins/ci.groovy
@@ -0,0 +1,13 @@
+@Library('add-ons-shared-libs@develop') _
+
+node {
+    continuousIntegrationPipeline(
+        buildType: "deploy",
+        sonar: [
+            enable: true,
+            projectKey: "eclipse-kura_kura-position",
+            tokenId: "sonarcloud-token-kura-position",
+            exclusions: "tests/**/*.java"
+        ],
+    )
+}
